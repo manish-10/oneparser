e8254fd336136056f03f3605abb9d922694414d1
Aayush Kumar <ayushk780@gmail.com>
2021-03-09 14:15:27 +0000

    navbar and index updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,11 @@
-
+import Link from 'next/link'
 const Navbar=()=>{
     return (
         <div>
-            <div className="container">
-            <div className=" text-xl bg-gradient-to-r from-green-400 to-blue-500 text-white py-4 px-4 font-bold">
-                <div className="text-black hover:text-white">OneParser</div>
-            </div>
-            
-            
+            <div className="container text-center">
+            <div className="text-xl bg-gradient-to-r from-green-400 to-blue-500 py-4 px-4 font-bold text-black hover:text-white">
+                <Link href="/" as="https://oneparser-1.ayushk1804.repl.co/">OneParser</Link>
+            </div>            
         </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,25 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { Textarea } from '../components/Textarea'
 
 export default function Home() {
-  const supportedLanguages=["a","2","3"];
-  const [value, setValue] = useState(supportedLanguages[0]);
-  return (
-    
-
-  <div className="grid grid-cols-12 gap-7 p-7">
-    
-    
-    <div className="col-span-6 text-center">
-    <select className="border px-6 " value={value} onChange={(e)=>setValue(e.target.value)}>
-      {supportedLanguages.map(lang=>(<option value={lang}>{lang}</option>))}
-    </select>
-    <Textarea disabled={false} title="Input"/>
-    </div>
-    <div className="col-span-6 text-center">
-    <select className="border px-6 " value={value} onChange={(e)=>setValue(e.target.value)}>
-    {supportedLanguages.map(lang=>(<option value={lang}>{lang}</option>))}
-    </select>
-    <Textarea disabled={true} title="Output"/>
-    </div>
-  </div>
-    
-  )
+    const supportedInputLanguages = ["a", "2", "3"];
+    const supportedOutputLanguages = ["o", "2", "3"]
+    const [valueInput, setValueInput] = useState(supportedInputLanguages[0]);
+    const [valueOutput, setValueOutput] = useState(supportedOutputLanguages[0]);
+    return (
+        <div className="grid grid-cols-12 gap-7 p-7">
+            <div className="col-span-6 text-center">
+                <select className="border px-6 " value={valueInput} onChange={(e) => setValueInput(e.target.value)}>
+                    {supportedInputLanguages.filter((lang) => lang !== valueOutput).map(lang => (<option key={lang} value={lang}>{lang}</option>))}
+                </select>
+                <Textarea disabled={false} title="Input" />
+            </div>
+            <div className="col-span-6 text-center">
+                <select className="border px-6 " value={valueOutput} onChange={(e) => setValueOutput(e.target.value)}>
+                    {supportedOutputLanguages.filter((lang) => lang !== valueInput).map(lang => (<option key={lang} value={lang}>{lang}</option>))}
+                </select>
+                <Textarea disabled={true} title="Output" />
+            </div>
+        </div>
+    )
 }
