57d428d7d8bb53eaaed88c494e97a3b5b1abdf0c
Aayush Kumar <ayushk780@gmail.com>
2021-03-12 21:26:44 +0000

    fix python indentation error

--- api/pandoc.py
+++ api/pandoc.py
@@ -13,18 +13,18 @@ CORS(app)
 @app.route("/api/pandoc", methods=["GET", "POST"])
 def pandoc(request):
     if request.method == "POST":
-			try:    
-        text = str(request.json['inputData'])
-        with open('stext','w') as sft:
-            sft.write(text)
-        
-        cmd = './'+str(request.json['cmd'])
-        os.system(cmd)
-        with open('otext', 'r') as oft:
-            contents = oft.read()
-        return response.json(contents)
-			except Exception as e:
-				return response.json(status=500, content="Request failed please contact admins")
+        try:    
+            text = str(request.json['inputData'])
+            with open('stext','w') as sft:
+                sft.write(text)
+            
+            cmd = './'+str(request.json['cmd'])
+            os.system(cmd)
+            with open('otext', 'r') as oft:
+                contents = oft.read()
+            return response.json(contents)
+        except Exception as e:
+            return response.json(status=500, content="Request failed please contact admins")
 
     if request.method == "GET":
         return response.json("Hello")
