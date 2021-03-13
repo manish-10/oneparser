3a29f75410b37c3e50789fdcb7d250156391cc86
Aayush Kumar <ayushk780@gmail.com>
2021-03-13 07:15:58 +0000

    fix api indentation error

--- api/speedtest.py
+++ api/speedtest.py
@@ -5,10 +5,10 @@ import speedtest
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-		s = speedtest.Speedtest()
-		s.get_best_server()
-		s.download()
-		s.upload()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
