638d64071703586aebe6bb164d51af67397c061a
Aayush Kumar <ayushk780@gmail.com>
2021-03-13 07:24:32 +0000

    added new json resp

--- api/speedtest.py
+++ api/speedtest.py
@@ -1,16 +1,25 @@
-from http.server import BaseHTTPRequestHandler
-import os
 import speedtest
 
-class handler(BaseHTTPRequestHandler):
+from sanic import Sanic, response
+from sanic_cors import CORS
 
-  def do_GET(self):
-    s = speedtest.Speedtest()
-    s.get_best_server()
-    s.download()
-    s.upload()
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(s.results.dict())
-    return
+app = Sanic(__name__)
+CORS(app)
+
+@app.route("/api/speedtest", methods=["GET"])
+def pandoc(request):
+    if request.method == "GET":
+        s = speedtest.Speedtest()
+        s.get_best_server()
+        s.download()
+        s.upload()
+        rstest=s.results.dict()
+        download_speed = rstest.get("download")
+        upload_speed = rstest.get("upload")
+        ping_time = rstest.get("ping")
+        client_infos = rstest.get("client")
+        i_s_p = client_infos.get("isp")
+        i_s_p_rating = client_infos.get("isprating")
+        return response.json({download_speed, upload_speed, client_infos, i_s_p, i_s_p_rating})
+
+app.run()
